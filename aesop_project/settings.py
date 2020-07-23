a157da3b223d75ff58f16392fc7bc9917e5296a4
hongalLee <dlghddlf1992@gmail.com>
2020-07-23 16:53:42 +0900

    model 추가

--- aesop_project/settings.py
+++ aesop_project/settings.py
@@ -41,7 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'board',
     'user',
-    'main'
+    'main',
+    'crawling'
 ]
 
 MIDDLEWARE = [
--- main/models.py
+++ main/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+class Catalog(models.Model):
+    name = models.CharField(max_length = 255)
+    is_activated = models.CharField(max_length = 10, default='Y', null=False, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField()
+
+    class Meta:
+        db_table = 'catalogs'
+
+    def __str__(self):
+        return self.name
+
+class Category(models.Model):
+    catalog = models.ForeignKey(Catalog, on_delete = models.CASCADE)
+    name = models.CharField(max_length = 255)
+    is_activated = models.CharField(max_length = 10, default='Y', null=False, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField()
+
+    class Meta:
+        db_table = 'categories'
+
+    def __str__(self):
+        return self.name
+
