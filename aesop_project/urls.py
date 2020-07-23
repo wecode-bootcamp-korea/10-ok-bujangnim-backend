86322b0a16949443ca12801196f1245472d684ec
exid0429 <exid0429@gmail.com>
2020-07-23 18:02:36 +0900

    ok

--- aesop_project/urls.py
+++ aesop_project/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     #path('admin/', admin.site.urls),
     path('crawling', include('crawling.urls'))
+    path('user',include('user.urls'))
 ]
--- user/views.py
+++ user/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
-
+from user.models import User
+from django.views import View
+from django.http import JsonResponse
 # Create your views here.
+
+class SignUpView(models.Model):
+    def post(self,request):
+        data = json.loads(request.body)
+        try:
+            if User.objects.filter(email = data['email']).exists():
+                return JsonResponse({'message' : 'Existing-Account'}, status = 401)
+            if('@' in data['email']) and( len(data['password']) >= 5):
+               User(
+                   first_name = data['first_name'],
+                   last_name = data['last_name'],
+                   password = data['password']
+               ).save()
+               return JsonResponse({'message' : 'success'}, status = 200)
+            return JsonResponse({'message' : 'Wrong_ERROR'}, status = 401)
+        except KeyError:
+            return JsonResponse({'message' :'KEY_ERROR'},status = 400)
+
+class SignInView(models.Model):
+    def post(self, request):
+        data = json.loads(request.body)
+        try:
+            if User.objects.filter(email = data['email']).exists():
+                user = User.objects.get(email = data['email'])
+            if user.password == data['password']:
+                return JsonResponse({'message' : 'success'}, status = 200)
+            return JsonResponse({'message': ' 허락안함'},status = 401)
+        except KeyError:
+            return JsonResponse({'message' :'오타'}, status = 400)
+
